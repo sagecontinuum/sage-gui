7e5861783cdfd28c79f8ca3274131b7d66a6b23a
nconrad <1194246+nconrad@users.noreply.github.com>
2021-02-11 15:56:29 -0600

    fancy zoom

--- components/Map.tsx
+++ components/Map.tsx
@@ -4,14 +4,17 @@ import styled from 'styled-components'
 import mapboxgl from 'mapbox-gl'
 import token from '../mapbox-token'
 
+const center = [-100, 50]
+const initialZoom = 1.5
+
 const loadMap = (domRef) => {
 	mapboxgl.accessToken = token
 
   const map = new mapboxgl.Map({
       container: domRef.current,
       style: 'mapbox://styles/mapbox/light-v10',
-      center: [-98, 39],
-      zoom: 2.95
+      center,
+      zoom: initialZoom
   })
 
   map.addControl(new mapboxgl.FullscreenControl({container: domRef.current}));
@@ -45,6 +48,48 @@ const renderMarkers = (map, data) => {
 }
 
 
+const recenter = (map) => {
+  flyTo(map, center[0], center[1], initialZoom)
+}
+
+
+const flyTo = (map, lon, lat, zoom = 10) => {
+  map.flyTo({
+    center: [lon, lat],
+    essential: true,
+    zoom
+  })
+}
+
+
+const fitBounds = (map, bbox) => {
+  map.fitBounds(bbox, {
+    padding: {top: 10, bottom:25, left: 15, right: 5},
+    maxZoom: 10
+  })
+}
+
+
+const getBBox = (coordinates) => {
+  const lons = coordinates.map(o => o.lon)
+  const lats = coordinates.map(o => o.lat)
+
+  const lonMin = Math.min(...lons)
+  const lonMax = Math.max(...lons)
+  const latMin = Math.min(...lats)
+  const latMax = Math.max(...lats)
+
+  return [[lonMin, latMin], [lonMax, latMax]]
+}
+
+
+
+const getValidCoords = (data) =>
+  data.filter(({Lat, Lon}) => Lat && Lon && Lat != 'N/A' && Lon != 'N/A')
+    .map(({Lat, Lon}) => ({lon: Lon, lat: Lat}))
+
+
+
 
 const getGeoSpec = (data) => {
   const nodes = data
@@ -73,16 +118,19 @@ const mapStyles = {
 
 
 type Props = {
-  data: object[]
+  data: {Lon: number, Lat: number}[] // todo: update definition with actual schema
 }
 
+
 function Map(props: Props) {
-  const {data} = props
+  const {data = null} = props
 
   const ref = useRef()
 
+  const [init, setInit] = useState(false)
   const [map, setMap] = useState()
   const [markers, setMarkers] = useState()
+  const [total, setTotal] = useState(null)
 
   useEffect(() => {
     const map = loadMap(ref)
@@ -92,18 +140,32 @@ function Map(props: Props) {
   useEffect(() => {
     if (!data || !map) return
 
-    // remove markers (todo: probably should use layers)
+    if (init && data != total) {
+      const coords = getValidCoords(data)
+      const bbox = getBBox(coords)
+      fitBounds(map, bbox)
+    } else {
+      recenter(map)
+    }
+
+    // remove markers (todo: probably should use layers?)
     (markers || []).forEach(marker => marker.remove())
 
     const geoSpec = getGeoSpec(data)
     const newMarkers = renderMarkers(map, geoSpec)
     setMarkers(newMarkers)
+
+    setInit(true)
+    setTotal(data.length)
   }, [data, map])
 
   return (
     <Root>
       <div id="map"
-        ref={ref} style={mapStyles}></div>
+        ref={ref}
+        style={mapStyles}
+      >
+      </div>
     </Root>
   )
 }
