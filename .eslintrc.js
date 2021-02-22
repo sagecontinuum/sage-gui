b46c9c284812fb4a3b7c1d3d1e5f920c309d0852
nconrad <1194246+nconrad@users.noreply.github.com>
2021-02-22 17:19:20 -0600

    some configuration (WIP)

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,8 @@ module.exports = {
   'extends': [
     'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:react-hooks/recommended'
+    'plugin:react-hooks/recommended',
+    "plugin:prettier/recommended"
   ],
   'parser': '@typescript-eslint/parser',
   'parserOptions': {
