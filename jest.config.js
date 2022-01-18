d4d5064550e9fb2e2d89d720e27989157e97e73f
nconrad <1194246+nconrad@users.noreply.github.com>
2022-01-18 10:32:17 -0600

    build: add react svg transformer

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,8 @@ module.exports = {
   moduleFileExtensions: ['ts', 'js', 'jsx', 'tsx'],
   collectCoverage: true,
   clearMocks: true,
-  coverageDirectory: 'coverage'
+  coverageDirectory: 'coverage',
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/jest/svgMock.js"
+  }
 }
---
+++ jest/svgMock.js
@@ -0,0 +1,2 @@
+
+module.exports = 'some-svg'
