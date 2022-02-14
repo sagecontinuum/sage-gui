cb21a771d4817fa60d80de8ff09e66f9dab08138
nconrad <1194246+nconrad@users.noreply.github.com>
2022-02-14 17:03:52 -0600

    build: add parcel module resolution type; update configs to support tilda in module referencing

--- jest.config.js
+++ jest.config.js
@@ -6,7 +6,8 @@ module.exports = {
   clearMocks: true,
   coverageDirectory: 'coverage',
   moduleNameMapper: {
-    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/svgMock.js"
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js",
+    "^~/(.*)": "<rootDir>/$1"
   },
   modulePathIgnorePatterns: [
     "<rootDir>/sage/_example/"
