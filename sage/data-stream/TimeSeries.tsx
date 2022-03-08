49aa0714001e2c4c133be0bc80b28c1119336e7a
nconrad <1194246+nconrad@users.noreply.github.com>
2022-03-07 20:20:47 -0600

    data: add more chart options

--- sage/data-stream/TimeSeries.tsx
+++ sage/data-stream/TimeSeries.tsx
@@ -1,20 +1,25 @@
 import { useState, useEffect } from 'react'
+import styled from 'styled-components'
 import { schemeCategory10 } from 'd3-scale-chromatic'
-import { Line } from 'react-chartjs-2'
-import { groupBy } from 'lodash'
+import { Line, Bar } from 'react-chartjs-2'
+import { chain, groupBy, countBy, sortBy, sumBy } from 'lodash'
 
 import * as BH from '~/components/apis/beehive'
 import FormControlLabel from '@mui/material/FormControlLabel'
 import Checkbox from '~/components/input/Checkbox'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import MenuItem from '@mui/material/MenuItem'
+import InputLabel from '@mui/material/InputLabel'
 
 
-function getChartDatasets(data: BH.Record[], showPoints: boolean) {
+function getLineDatasets(records: BH.Record[], opts: ChartOpts) {
   const datasets = []
 
-  const byName = groupBy(data, 'name')
+  const byName = groupBy(records, 'name')
 
   let idx = 0
-  Object.keys(byName).forEach((name, i) => {
+  Object.keys(byName).forEach((name) => {
     const namedData = byName[name]
 
     const grouped = groupBy(namedData, 'sensor')
@@ -30,9 +35,9 @@ function getChartDatasets(data: BH.Record[], showPoints: boolean) {
         datasets.push({
           label: name + (hasSesnors ? ` - ${key}` : ''),
           data: d,
-          pointRadius: showPoints ? 3 : 0,
+          pointRadius: opts.showPoints ? 3 : 0,
           fill: false,
-          showLine: false,
+          showLine: opts.showLines,
           tension: 0,
           borderColor: schemeCategory10[idx % 10]
         })
@@ -45,47 +50,213 @@ function getChartDatasets(data: BH.Record[], showPoints: boolean) {
 }
 
 
+function getCountData(records: BH.Record[], opts: ChartOpts) {
+  const items = chain(records)
+    .countBy('name')
+    .map((val, key) => ({
+      name: key,
+      value: val
+    }))
+    .sortBy('value')
+    .reverse()
+    .value()
+
+  const labels = items.map(o => o.name)
+  const data = items.map(o => o.value)
+
+  return {
+    labels: labels,
+    datasets: [{
+      label: 'frequency',
+      data,
+      backgroundColor: labels.map((_, i) => schemeCategory10[i % 10])
+    }]
+  }
+}
+
+
+function getSumData(records: BH.Record[], opts: ChartOpts) {
+  const items = chain(records)
+    .groupBy('name')
+    .map((objs, key) => ({
+      name: key,
+      value: sumBy(objs, 'value')
+    }))
+    .sortBy('value')
+    .reverse()
+    .value()
+
+  const labels = items.map(o => o.name)
+  const data = items.map(o => o.value)
+
+  return {
+    labels: labels,
+    datasets: [{
+      label: 'sums',
+      data,
+      backgroundColor: labels.map((_, i) => schemeCategory10[i % 10])
+    }]
+  }
+}
+
+
+
+type ChartOpts = {
+  showLines: boolean
+  showPoints: boolean
+  chartType: 'timeseries' | 'frequency' | 'sum'
+}
+
+const chartOpts = {
+  showLines: true,
+  showPoints: false,
+  chartType: 'timeseries'
+}
+
+
 export default function TimeSeries(props) {
   const {data} = props
 
   const [datasets, setDatasets] = useState<object[]>()
-  const [showPoints, setShowPoints] = useState(false)
+  const [opts, setOpts] = useState<ChartOpts>(chartOpts)
 
 
   useEffect(() => {
-    const datasets = getChartDatasets(data, showPoints)
+    let datasets
+    if (opts.chartType == 'frequency')
+      datasets = getCountData(data, opts)
+    else if (opts.chartType == 'sum')
+      datasets = getSumData(data, opts)
+    else
+      datasets = getLineDatasets(data, opts)
+
     setDatasets(datasets)
-  }, [data, showPoints])
+  }, [data, opts])
+
+
+  const handleOpts = (evt, option: string) => {
+    const target = evt.target
+    const val = 'checked' in target ? target.checked : target.value
+    setOpts(prev => ({...prev, [option]: val}))
+  }
 
   return (
-    <div>
-      <Line
-        options={{
-          scales: {
-            xAxes: [{
-              type: 'time',
-              display: true,
-              scaleLabel: {
-                display: true
+    <Root>
+      <Chart>
+        {opts.chartType == 'timeseries' && Array.isArray(datasets) &&
+          <Line
+            options={{
+              scales: {
+                xAxes: [{
+                  type: 'time',
+                  display: true,
+                  scaleLabel: {
+                    display: true
+                  }
+                }]
               }
-            }]
-          }
-        }}
-        data={{
-          datasets
-        }}
-      />
-      <FormControlLabel
-        control={
-          <Checkbox
-            checked={showPoints}
-            onChange={(evt) => setShowPoints(evt.target.checked)}
+            }}
+            data={{
+              datasets
+            }}
           />
         }
-        label="show points"
-      />
-    </div>
+
+        {opts.chartType == 'frequency' &&
+          <Bar
+            options={{
+              scales: {
+                yAxes: [{
+                    display: true,
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+              }
+            }}
+            data={{
+              ...datasets
+            }}
+          />
+        }
+
+        {opts.chartType == 'sum' &&
+          <Bar
+            options={{
+              scales: {
+                yAxes: [{
+                    display: true,
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+              }
+            }}
+            data={{
+              ...datasets
+            }}
+          />
+        }
+      </Chart>
+
+
+      <div className="flex justify-end">
+        {opts.chartType == 'timeseries' &&
+          <>
+          <CB
+            control={
+              <Checkbox
+                checked={opts.showLines}
+                onChange={(evt) => handleOpts(evt, 'showLines')}
+              />
+            }
+            label="lines"
+          />
+          <CB
+            control={
+              <Checkbox
+                checked={opts.showPoints}
+                onChange={(evt) => handleOpts(evt, 'showPoints')}
+              />
+            }
+            label="points"
+          />
+          </>
+        }
+
+        <FormControl variant="outlined">
+          <InputLabel id="chart-type">Type</InputLabel>
+          <Select
+            labelId="chart-type"
+            value={opts.chartType}
+            onChange={evt => handleOpts(evt, 'chartType')}
+            label="Type"
+            margin="dense"
+          >
+            <MenuItem value='timeseries'>Timeseries</MenuItem>
+            <MenuItem value='frequency'>Frequency</MenuItem>
+            <MenuItem value='sum'>Sum</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+    </Root>
   )
 
 }
 
+
+const Root = styled.div`
+  border-bottom: 1px solid #f1f1f1;
+  padding-bottom: 1em;
+`
+
+const Chart = styled.div`
+
+`
+
+const CB = styled(FormControlLabel)`
+  .MuiFormControlLabel-label {
+    font-size: 1em;
+  }
+
+`
