0193c8074fe3fc6d8cd4d51f271c692fb857dc5d
nconrad <1194246+nconrad@users.noreply.github.com>
2022-04-29 14:30:45 -0500

    add env variable support for selecting configs at build time

---
+++ apps/common/settings.ts
@@ -0,0 +1,5 @@
+export default {
+  // currently used for customm sensors such as Met One;
+  // todo(nc): this should be removed when we have manifests
+  mdpNodes: ['W038', 'W01D', 'W01F']
+}
--- apps/project/settings.ts
+++ apps/project/settings.ts
@@ -1,6 +1,23 @@
 
+// use env variable to load corresponding config below
+const SAGE_UI_PROJECT = process.env.SAGE_UI_PROJECT
+
+const configs = {
+  'dawn': {
+    logo: 'DAWN',
+    project: 'DAWN'
+  },
+  'neon-mdp': {
+    logo: 'NEON-MDP',
+    project: 'SAGE',
+    focus: 'NEON-MDP'
+  }
+}
+
 export default {
-  logo: 'NEON-MDP',
-  project: 'SAGE',
-  focus: 'NEON-MDP'
+  elapsedThresholds: {
+    fail: 360000,
+    warning: 180000
+  },
+  ...configs[SAGE_UI_PROJECT.toLowerCase()]
 }
