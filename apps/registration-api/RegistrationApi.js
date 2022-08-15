3c3b1cdf5ccd312fc9a82a43ac608d30d7827a88
Sammi Huang <96802164+SammiH2022@users.noreply.github.com>
2022-08-15 10:42:20 -0500

    fix(reg-api): replace key script route on docker

--- apps/registration-api/RegistrationApi.js
+++ apps/registration-api/RegistrationApi.js
@@ -19,7 +19,7 @@ app.get("/register", (req, res) => {
   const tmpDir = tmpObj.name;
 
   exec(
-    `${__dirname}/create-key-cert.sh -b beehive-dev -e +1d -o ${tmpDir} -c ${__dirname}/beekeeper-keys/certca/beekeeper_ca_key`,
+    `/usr/bin/create-key-cert.sh -b beehive-dev -e +1d -o ${tmpDir} -c ${__dirname}/beekeeper-keys/certca/beekeeper_ca_key`,
     (err, stdout, stderr) => {
       if (err !== null) {
         return res.status(400).json({ error: err.message });
