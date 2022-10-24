70ffbf175df6aa611cb99330f6d718daf76b646c
nconrad <1194246+nconrad@users.noreply.github.com>
2022-10-24 18:48:39 -0500

    feat: revised map component; add tooltip links

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,121 +1,38 @@
 import { useEffect, useRef, useState } from 'react'
+import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
-import mapboxgl from 'mapbox-gl'
-import config from '/config'
-import 'mapbox-gl/dist/mapbox-gl.css'
+import Map, {
+  Marker, Popup, Source, Layer,
+  FullscreenControl, NavigationControl
+} from 'react-map-gl'
+import type { MapRef } from 'react-map-gl'
 
-// import Popover from '@mui/material/Popover'
+import { ClickAwayListener } from '@mui/material'
+
+import Clipboard from './utils/Clipboard'
+import type { Manifest } from '/components/apis/beekeeper'
+import config from '/config'
+
 
 const DISABLE_MAP = config['disableMaps'] || false
-const center = [-99, 38.5]
-const initialZoom = 3.0
+const MAPBOX_TOKEN = process.env.MAPBOX_TOKEN || 'xyz.123'
+
 const dotSize = 10
 
-const minHeight = '225px'
-const maxHeight = '350px'
-
-
-const loadMap = (domRef, resize) => {
-  mapboxgl.accessToken = process.env.MAPBOX_TOKEN || 'xyz.123'
-
-  const map = new mapboxgl.Map({
-    container: domRef.current,
-    style: 'mapbox://styles/mapbox/light-v9',
-    center,
-    zoom: initialZoom
-  })
-
-  if (resize) {
-    map.on('load', () => {
-      map.resize()
-    })
-  }
-
-  map.addControl(new mapboxgl.FullscreenControl({container: domRef.current}))
-
-  return map
+const mapSettings = {
+  initialViewState: {
+    longitude: -99,
+    latitude: 38.5,
+    zoom: 3.0
+  },
+  mapboxAccessToken: MAPBOX_TOKEN,
+  style: {height: '350px'},
+  mapStyle: 'mapbox://styles/mapbox/light-v9',
+  cooperativeGestures: true
 }
 
-
-
-const renderMarkers = (map, data) => {
-  const markers = data.features.map(marker => {
-    const {data} = marker.properties
-    // todo: optimize popups by using 'onclick'
-    var el = document.createElement('div')
-    el.className = 'marker'
-    el.innerHTML = `<div class="marker-dot marker-${marker.status.replace(' ', '-')}"></div>`
-
-    return new mapboxgl.Marker(el).setLngLat(marker.geometry.coordinates)
-      .addTo(map)
-      .setPopup(
-        new mapboxgl.Popup({ offset: dotSize + 2 })
-          .setHTML(
-            `<h3 class="popup-title">${marker.properties.title}</h3>
-            <table class="key-value simple">
-              <tr><td>Project</td><td>${data.project} (${data.focus})</td><tr>
-              <tr><td>Location</td><td>${data.location}</td><tr>
-              <tr><td>ID</td><td>${data.id}</td><tr>
-              <tr><td>Coordinates</td><td>${data.lat}<br>${data.lng}</td><tr>
-            </table>
-            <p>${marker.properties.description}</p>`
-          )
-      )
-  })
-
-  return markers
-}
-
-const renderLabels = (map, geoSpec) => {
-  map.addSource('geoSpec', {
-    'type': 'geojson',
-    'data': geoSpec
-  })
-
-  map.addLayer({
-    'id': 'marker-labels',
-    'type': 'symbol',
-    'source': 'geoSpec',
-    'layout': {
-      'text-field': [
-        'format',
-        ['upcase', ['get', 'title']],
-        { 'font-scale': 0.9 },
-      ],
-      'text-variable-anchor': ['top', 'bottom', 'left', 'right'],
-      'text-radial-offset': 0.5,
-      'text-justify': 'auto',
-      'icon-image': ['get', 'icon'],
-    }
-  })
-}
-
-
-const clearMarkers = (markers) =>
-  (markers || []).forEach(marker => marker.remove())
-
-
-const recenter = (map) => {
-  flyTo(map, center[0], center[1], initialZoom)
-}
-
-
-const flyTo = (map, lng, lat, zoom = 10) => {
-  map.flyTo({
-    center: [lng, lat],
-    essential: true,
-    zoom
-  })
-}
-
-
-const fitBounds = (map, bbox) => {
-  map.fitBounds(bbox, {
-    padding: {top: 35, bottom: 35, left: 35, right: 35},
-    maxZoom: 10
-  })
-}
+const fitBoundsPadding = {top: 35, bottom: 35, left: 35, right: 35}
 
 
 const getBBox = (coordinates) => {
@@ -135,7 +52,6 @@ const getBBox = (coordinates) => {
 const getValidCoords = (data) =>
   data.filter(({lat, lng}) => lat && lng && lat != 'N/A' && lng != 'N/A')
     .map(({lat, lng}) => ({lng, lat}))
-
 
 
 
@@ -163,161 +79,182 @@ const getGeoSpec = (data) => {
 }
 
 
+const layerStyle = {
+  'id': 'marker-labels',
+  'type': 'symbol',
+  'source': 'geoSpec',
+  'layout': {
+    'text-field': [
+      'format',
+      ['upcase', ['get', 'title']],
+      { 'font-scale': 0.9 },
+    ],
+    'text-variable-anchor': ['top', 'bottom', 'left', 'right'],
+    'text-radial-offset': 0.5,
+    'text-justify': 'auto',
+    'icon-image': ['get', 'icon'],
+  }
+}
 
-type Data = {id: string, lng: number, lat: number}[]
+
+type VSN = `W${string}` | `V${string}`
+
+type Data = {
+  id: string,
+  vsn: VSN ,
+  lng: number,
+  lat: number,
+  status?: string
+}[]
 
 type Props = {
   data: Data
-  selected: Data
   updateID: number
-  vsn: `W${string}` | `V${string}`
+  vsn?: VSN
   resize?: boolean
 }
 
 
-function Map(props: Props) {
+export default function MapGL(props: Props) {
   const {
     data = null,
-    selected,
-    updateID,
-    resize = true
+    updateID
   } = props
 
-  const ref = useRef()
-  const [init, setInit] = useState(false)
-  const [map, setMap] = useState(null)
-  const [markers, setMarkers] = useState()
-  const [total, setTotal] = useState(null)
+  const mapRef = useRef<MapRef>(null)
+  const [popup, setPopup] = useState(null)
+
+  const [geoData, setGeoData] = useState(null)
+  const [markers, setMarkers] = useState([])
 
   // keep track of primary keys
   const [lastID, setLastID] = useState(-1)
 
-  // popover content
-  // const [popup, setPopup] = useState<string>(false)
-
-
+  // just determine when to update for now
   useEffect(() => {
     if (DISABLE_MAP) return
-
-    const map = loadMap(ref, resize)
-    setMap(map)
-
-    if (!resize) return
-
-    window.onscroll = () => {
-      if (document.documentElement.scrollTop > 0) {
-        document.getElementById('map').style.height = minHeight
-      } else {
-        document.getElementById('map').style.height = maxHeight
-      }
-
-      setTimeout(() => {
-        map.resize()
-      }, 300)
-    }
-  }, [])
-
-  useEffect(() => {
-    if (!map) return
-    updateMap()
-  }, [map, data, selected, updateID])
-
-  const updateMap = () => {
-    if (!data || !map)
-      return
-
-    // if no matches, just clear the map
-    if (!data.length) {
-      clearMarkers(markers)
-      return
-    }
 
     // do a manual comparison for dynamic ping updates
     if (lastID == updateID) {
       return
     }
 
-    // filter selected if needed
-    const filteredData = selected ?
-      data.filter(obj => selected.map(o => o.id).includes(obj.id) ) : data
+    const d = data
+    const geoData = getGeoSpec(d)
+    const coords = getValidCoords(d)
+    const bbox = getBBox(coords)
 
-    // fly to box containing new data
-    if (init && data != total) {
-      const coords = getValidCoords(filteredData)
+    setGeoData(geoData)
+    setMarkers(d)
 
-      if (coords.length) {
-        const bbox = getBBox(coords)
-        fitBounds(map, bbox)
-      } else {
-        recenter(map)
-      }
-    } else {
-      recenter(map)
+    if (mapRef.current && coords.length) {
+      mapRef.current.fitBounds(bbox, { padding: fitBoundsPadding, maxZoom: 10 })
     }
 
-    // remove markers/labels (todo: use layers for markers too?)
-    clearMarkers(markers)
-
-    if(map.getLayer('marker-labels')) {
-      map.removeLayer('marker-labels')
-        .removeSource('geoSpec')
-    }
-
-    // add new markers
-    const geoSpec = getGeoSpec(filteredData)
-    const newMarkers = renderMarkers(map, geoSpec)
-
-    try {
-      renderLabels(map, geoSpec)
-    } catch(e) {
-      // need delay for initial styling load
-      map.on('load', () => {
-        renderLabels(map, geoSpec)
-      })
-    }
-
-    document.querySelectorAll('.mapboxgl-marker').forEach(el =>
-      el.addEventListener('click', (evt) => {
-      })
-    )
-
-    setMarkers(newMarkers)
-    setInit(true)
-    setTotal(data.length)
     setLastID(updateID)
+  }, [data, updateID, lastID])
+
+
+  const handleClick = (evt, obj) => {
+    evt.originalEvent.stopPropagation()
+    setPopup(obj)
   }
+
+
+  // tooltip info deconstruction
+  const {
+    vsn, project, focus, location, lng, lat, node_type
+  }: Manifest = popup || {}
+
 
   return (
     <Root>
-      <MapContainer id="map" ref={ref} />
+      <Map ref={mapRef} {...mapSettings}>
+        <FullscreenControl />
+        <NavigationControl position="bottom-right" showCompass={false} />
 
-      {/* todo: implement?
-      <Popover
-        open={!!popover}
-        anchorOrigin={{
-          vertical: 'top',
-        }}
-        transformOrigin={{
-          vertical: 'top',
-        }}
-      >
-        {popover}
-      </Popover>
-      */}
+        {geoData &&
+          <Source id="markers" type="geojson" data={geoData}>
+            <Layer {...layerStyle} />
+          </Source>
+        }
+
+        {markers.map(obj => {
+          const {vsn, lng, lat, status} = obj
+
+          if (!lng || !lat)
+            return <></>
+
+          return (
+            <Marker key={vsn}
+              longitude={lng}
+              latitude={lat}
+              onClick={(evt) => handleClick(evt, obj)}
+            >
+              <div className={`marker-dot marker-${status}`}></div>
+            </Marker>
+          )
+        })}
+
+        {vsn &&
+          <Popup
+            longitude={lng}
+            latitude={lat}
+            onClose={() => setPopup(null)}
+          >
+            <ClickAwayListener onClickAway={() => setPopup(null)}>
+              <div>
+
+                <h2>
+                  {node_type == 'WSN' ?
+                    'Wild Sage Node' : node_type
+                  } <Link to={`/node/${popup.node_id}`}>
+                    {popup.vsn}
+                  </Link>
+                </h2>
+                <table className="key-value simple">
+                  <tbody>
+                    <tr>
+                      <td>Project</td>
+                      <td>
+                        <Link to={`/nodes/?project="${encodeURIComponent(project)}"`}>
+                          {project}
+                        </Link>&nbsp;
+                      (<Link to={
+                          `/nodes/?project="${encodeURIComponent(project)}"` +
+                        `&focus="${encodeURIComponent(focus)}"`
+                        }>
+                          {focus}
+                        </Link>)
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>Location</td>
+                      <td>
+                        <Link to={`/nodes/?location="${encodeURIComponent(location)}"`}>
+                          {location}
+                        </Link>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>Coordinates</td>
+                      <td><Clipboard content={`${lat},\n${lng}`} /></td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </ClickAwayListener>
+          </Popup>}
+      </Map>
+
     </Root>
   )
 }
 
+
 const Root = styled.div`
   width: 100%;
   flex-grow: 4;
-`
-
-
-const MapContainer = styled.div`
-  border: 1px solid #ccc;
-  width: 100%;
-  height: ${maxHeight};
 
   .popup-title {
     margin-top: 0;
@@ -349,7 +286,9 @@ const MapContainer = styled.div`
      background: #d72020;
      border: 1px solid #992727;
   }
+
+  table td:first-child {
+    width: 100px;
+    text-align: right;
+  }
 `
-
-
-export default Map
