c7c9abbdc849108d946a2ef762a103693bc660b6
FranciscoLozCoding <francisco.lozano33@outlook.com>
2023-10-12 16:19:09 -0500

    removed expected_uplink_interval_sec column

--- components/views/node/lorawandevice/columns.tsx
+++ components/views/node/lorawandevice/columns.tsx
@@ -1,19 +1,16 @@
 import * as formatters from '/components/views/node/lorawandevice/deviceFormatters'
 
-const lorawandeviceCols = [/*{
-    id: 'status',
+const lorawandeviceCols = [{
+    id: 'last_seen_at',
     label: 'Status',
     format: formatters.status,
     width: '1px'
-  },*/{
+  },{
     id: 'device_name',
     label: 'Name'
   }, {
     id: 'deveui',
     label: 'DevEUI'
-  }, {
-    id: 'last_seen_at',
-    label: 'Last Seen At'
   },
   {
     id: 'battery_level',
@@ -22,10 +19,6 @@ const lorawandeviceCols = [/*{
   {
     id: 'margin',
     label: 'Margin'
-  },
-  {
-    id: 'expected_uplink_interval_sec',
-    label: 'Expected uplink interval (sec)'
   }]
 
 
