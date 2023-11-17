2828ddfbe56ebc105b79205af549b2bb47f91470
nconrad <1194246+nconrad@users.noreply.github.com>
2023-11-17 17:36:05 -0600

    drop makeStyles from column selector in favor of emotion implementation

--- components/table/ColumnMenu.tsx
+++ components/table/ColumnMenu.tsx
@@ -1,7 +1,8 @@
-// @ts-nocheck: ignore for mui/styles theme typing issues (mui/styles is deprecated)
 import React, { useState } from 'react'
 import { useTheme, alpha } from '@mui/material/styles'
-import makeStyles from '@mui/styles/makeStyles'
+
+import { styled } from '@mui/material/styles'
+
 import Popper from '@mui/material/Popper'
 import SettingsIcon from '@mui/icons-material/SettingsOutlined'
 import DoneIcon from '@mui/icons-material/Done'
@@ -21,82 +22,42 @@ const options = [
 ]
 */
 
-const useStyles = makeStyles((theme) => ({
-  popper: {
-    border: '1px solid rgba(27,31,35,.15)',
-    boxShadow: '0 3px 12px rgba(27,31,35,.15)',
-    borderRadius: 3,
-    width: 300,
-    zIndex: 100,
-    fontSize: 13,
-    color: '#586069',
-    backgroundColor: '#f6f8fa'
-  },
-  header: {
-    borderBottom: '1px solid #e1e4e8',
-    padding: '8px 10px',
-    fontWeight: 600,
-  },
-  inputBase: {
-    padding: 10,
-    width: '100%',
-    borderBottom: '1px solid #dfe2e5',
-    '& input': {
-      borderRadius: 4,
-      backgroundColor: theme.palette.common.white,
-      padding: 8,
-      transition: theme.transitions.create(['border-color', 'box-shadow']),
-      border: '1px solid #ced4da',
-      fontSize: 14,
-      '&:focus': {
-        boxShadow: `${alpha(theme.palette.primary.main, 0.25)} 0 0 0 0.2rem`,
-        borderColor: theme.palette.primary.main,
-      },
-    },
-  },
-  paper: {
-    boxShadow: 'none',
-    margin: 0,
-    color: '#586069',
-    fontSize: 13
-  },
-  option: {
-    minHeight: 'auto',
-    alignItems: 'flex-start',
+const StyledPopper = styled(Popper)(() => ({
+  border: '1px solid rgba(27,31,35,.15)',
+  boxShadow: '0 3px 12px rgba(27,31,35,.15)',
+  borderRadius: 3,
+  width: 300,
+  zIndex: 100,
+  fontSize: 13,
+  color: '#586069',
+  backgroundColor: '#f6f8fa',
+}))
+
+const DivHeader = styled('div')(() => ({
+  borderBottom: '1px solid #e1e4e8',
+  padding: '8px 10px',
+  fontWeight: 600,
+}))
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  padding: 10,
+  width: '100%',
+  borderBottom: '1px solid #dfe2e5',
+
+  '& input': {
+    borderRadius: 4,
+    backgroundColor: theme.palette.common.white,
     padding: 8,
-    '&[aria-selected="true"]': {
-      backgroundColor: 'transparent',
+    transition: theme.transitions.create(['border-color', 'box-shadow']),
+    border: '1px solid #ced4da',
+    fontSize: 12,
+    '&:focus': {
+      boxShadow: `${alpha(theme.palette.primary.main, 0.25)} 0 0 0 0.2rem`,
+      borderColor: theme.palette.primary.main,
     },
-    '&[data-focus="true"]': {
-      backgroundColor: theme.palette.action.hover,
-    },
-  },
-  popperDisablePortal: {
-    position: 'relative',
-  },
-  iconSelected: {
-    width: 17,
-    height: 17,
-    marginRight: 5,
-    marginLeft: -2,
-  },
-  color: {
-    width: 14,
-    height: 14,
-    flexShrink: 0,
-    borderRadius: 3,
-    marginRight: 8,
-    marginTop: 2,
-  },
-  text: {
-    flexGrow: 1,
-  },
-  close: {
-    opacity: 0.6,
-    width: 18,
-    height: 18,
   },
 }))
+
 
 // todo(nc): figure out what is happening with typing here.
 type Option = {
@@ -125,7 +86,6 @@ export default function ColumnMenu(props: Props) {
     ...rest
   } = props
 
-  const classes = useStyles()
   const [anchorEl, setAnchorEl] = useState(null)
   const theme = useTheme()
 
@@ -165,27 +125,14 @@ export default function ColumnMenu(props: Props) {
         </Tooltip>
       }
 
-      <Popper
-        id={id}
-        open={open}
-        anchorEl={anchorEl}
-        placement="bottom-start"
-        className={classes.popper}
-      >
-        {header &&
-          <div className={classes.header}>
-            {headerText ? headerText : 'Show/hide columns'}
-          </div>
-        }
+      <StyledPopper id={id} open={open} anchorEl={anchorEl} placement="bottom-start">
+        {header && (
+          <DivHeader>{headerText ? headerText : 'Show/hide columns'}</DivHeader>
+        )}
         <Autocomplete
           open
           onClose={handleClose}
           multiple
-          classes={{
-            paper: classes.paper,
-            option: classes.option,
-            popperDisablePortal: classes.popperDisablePortal,
-          }}
           value={value}
           onChange={(event, newValue) => {
             setValue(newValue)
@@ -210,29 +157,28 @@ export default function ColumnMenu(props: Props) {
                   sx={{
                     flexGrow: 1,
                     '& span': {
-                      color:
-                      theme.palette.mode === 'light' ? '#586069' : '#8b949e',
+                      color: theme.palette.mode === 'light' ? '#586069' : '#8b949e',
                     },
+                    fontSize: '.9em'
                   }}
                 >
                   {option.label || option.id}
                 </Box>
               </li>
-            )}}
+            )
+          }}
           options={options}
           getOptionLabel={(option) => option.label || option.id}
           renderInput={(params) => (
-            <InputBase
+            <StyledInputBase
               ref={params.InputProps.ref}
               inputProps={params.inputProps}
               autoFocus
-              className={classes.inputBase}
             />
           )}
-
           {...rest}
         />
-      </Popper>
+      </StyledPopper>
     </div>
   )
 }
