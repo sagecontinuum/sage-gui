80d0da508c3fbc32bc01805b0f04677fde983891
FranciscoLozCoding <francisco.lozano33@outlook.com>
2024-03-22 15:00:25 -0500

    added new columns in lorawan tab

--- components/views/node/lorawandevice/columns.tsx
+++ components/views/node/lorawandevice/columns.tsx
@@ -1,33 +1,25 @@
-// import * as formatters from '/components/views/node/lorawandevice/deviceFormatters'
+import * as formatters from '/components/views/node/lorawandevice/deviceFormatters'
 
-// const lorawandeviceCols = [{
-//   id: 'last_seen_at',
-//   label: 'Status',
-//   format: formatters.status,
-//   width: '1px'
-// },{
-//   id: 'name',
-//   label: 'Name'
-// }, {
-//   id: 'deveui',
-//   label: 'DevEUI'
-// },
-// {
-//   id: 'battery_level',
-//   label: 'Battery Level'
-// },
-// {
-//   id: 'margin',
-//   label: 'Margin'
-// }]
-
-const lorawandeviceCols_temp = [{
+const lorawandeviceCols = [{
+  id: 'last_seen_at',
+  label: 'Status',
+  format: formatters.status,
+  width: '1px'
+},{
   id: 'name',
   label: 'Name'
 }, {
   id: 'deveui',
   label: 'DevEUI'
+},
+{
+  id: 'battery_level',
+  label: 'Battery Level'
+},
+{
+  id: 'margin',
+  label: 'Margin'
 }]
 
 
-export default lorawandeviceCols_temp
+export default lorawandeviceCols
