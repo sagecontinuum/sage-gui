4ae705f660d03bd9bd951e62d485d88f1b6617c2
FranciscoLozCoding <francisco.lozano33@outlook.com>
2024-03-25 16:44:53 -0500

    added tooltips and rssi column

--- components/views/node/lorawandevice/columns.tsx
+++ components/views/node/lorawandevice/columns.tsx
@@ -1,4 +1,6 @@
 import * as formatters from '/components/views/node/lorawandevice/deviceFormatters'
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined'
+import Tooltip from '@mui/material/Tooltip'
 
 const lorawandeviceCols = [{
   id: 'last_seen_at',
@@ -18,7 +20,24 @@ const lorawandeviceCols = [{
 },
 {
   id: 'margin',
-  label: 'Margin'
+  label: (
+    <Tooltip title="Represents the difference between Max measured SNR at the gateway and
+     Min SNR needed to demodulate a message at a specific data rate. It indicates the 
+     quality of the received signal compared to background noise." placement="bottom">
+      <span>Margin<InfoOutlinedIcon fontSize="small" /></span>
+    </Tooltip>
+  )
+},
+{
+  id: 'rssi',
+  label: (
+    <Tooltip title="Signal strength calculated from RSSI. 
+    Column disabled if no RSSI values are sent." placement="bottom">
+      <span>Signal Strength<InfoOutlinedIcon fontSize="small" /></span>
+    </Tooltip>
+  ),
+  format: formatters.signal,
+  width: '200px'
 }]
 
 
