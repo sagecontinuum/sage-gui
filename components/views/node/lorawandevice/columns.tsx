1eb748efa68001ad32f3aaa4eb5329c07ecef17f
FranciscoLozCoding <francisco.lozano33@outlook.com>
2024-03-26 15:40:10 -0500

    switched my labels with tooltips to use the new label with tooltip function

--- components/views/node/lorawandevice/columns.tsx
+++ components/views/node/lorawandevice/columns.tsx
@@ -1,6 +1,4 @@
 import * as formatters from '/components/views/node/lorawandevice/deviceFormatters'
-import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined'
-import Tooltip from '@mui/material/Tooltip'
 
 const lorawandeviceCols = [{
   id: 'last_seen_at',
@@ -21,44 +19,30 @@ const lorawandeviceCols = [{
 },
 {
   id: 'margin',
-  label: (
-    <Tooltip
-      title={
-        <span>
-          Indicates the quality of the received signal compared to background noise using the 
-          difference between Max measured 
-          <a 
-            href="https://www.thethingsnetwork.org/docs/lorawan/rssi-and-snr/#snr" 
-            target="_blank" 
-            rel="noreferrer"> SNR </a>  
-          at the gateway and Min SNR needed to demodulate a message at a specific data rate.
-        </span>
-      }
-      placement="bottom"
-    >
-      <span>Margin<InfoOutlinedIcon fontSize="small" /></span>
-    </Tooltip>
-  ),
+  label: formatters.LabelWithTooltip(
+    'Margin',
+    <span>
+      Indicates the quality of the received signal compared to background noise using the 
+      difference between Max measured 
+      <a 
+        href="https://www.thethingsnetwork.org/docs/lorawan/rssi-and-snr/#snr" 
+        target="_blank" 
+        rel="noreferrer"> SNR </a>  
+      at the gateway and Min SNR needed to demodulate a message at a specific data rate.
+    </span> )
 },
 {
   id: 'rssi',
-  label: (
-    <Tooltip
-      title={
-        <span>
-          Determined by 
-          <a 
-            href="https://www.thethingsnetwork.org/docs/lorawan/rssi-and-snr/#rssi" 
-            target="_blank" 
-            rel="noreferrer"> RSSI</a>. 
-          Column disabled if signal.rssi values are not being collected. 
-        </span>
-      }
-      placement="bottom"
-    >
-      <span>Signal Strength<InfoOutlinedIcon fontSize="small" /></span>
-    </Tooltip>
-  ),
+  label: formatters.LabelWithTooltip(
+    'Signal Strength',
+    <span>
+      Determined by 
+      <a 
+        href="https://www.thethingsnetwork.org/docs/lorawan/rssi-and-snr/#rssi" 
+        target="_blank" 
+        rel="noreferrer"> RSSI</a>. 
+      Column disabled if signal.rssi values are not being collected. 
+    </span> ),
   format: formatters.signal,
   width: '200px'
 }]
