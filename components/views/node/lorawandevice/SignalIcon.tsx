ea561904d48a87befb7cf1e5719fef9109e16cea
FranciscoLozCoding <francisco.lozano33@outlook.com>
2024-04-03 14:58:24 -0500

    switched to styled instead of makeStyles

--- components/views/node/lorawandevice/SignalIcon.tsx
+++ components/views/node/lorawandevice/SignalIcon.tsx
@@ -2,44 +2,41 @@ import SignalCellularAlt1BarIcon from '@mui/icons-material/SignalCellularAlt1Bar
 import SignalCellularAlt2BarIcon from '@mui/icons-material/SignalCellularAlt2Bar'
 import SignalCellularAltIcon from '@mui/icons-material/SignalCellularAlt'
 import Tooltip from '@mui/material/Tooltip'
-import { makeStyles } from '@mui/styles'
+import { styled } from '@mui/system'
 
-const useStyles = makeStyles(() => ({
-  iconWrapper: {
-    position: 'relative',
-    display: 'inline-block',
-  },
-  baseIcon: {
-    fill: 'none',
-    stroke: 'black',
-    strokeWidth: 0.5 
-  },
-  overlayIcon: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    zIndex: 1,
-  },
-}))
+const IconWrapper = styled('div')({
+  position: 'relative',
+  display: 'inline-block',
+})
 
-const SignalIcon = ({bars}) => {
-  const classes = useStyles()
+const BaseIcon = styled(SignalCellularAltIcon)({
+  fill: 'none',
+  stroke: 'black',
+  strokeWidth: 0.5,
+})
+
+const OverlayIcon = styled('div')({
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  zIndex: 1,
+})
+
+const SignalIcon = ({ bars }) => {
   const title =
     bars === 3 ? 'Strong Signal' :
       bars === 2 ? 'Moderate Signal' :
         bars === 1 ? 'Weak Signal' : 'No Signal'
   return (
-    <Tooltip
-      title={title}
-      componentsProps={{tooltip: { sx: { background: '#000' }}}}
-      placement="top"
-    >
-      <div className={classes.iconWrapper}>
-        <SignalCellularAltIcon className={classes.baseIcon} />
-        {bars === 3 && <SignalCellularAltIcon className={classes.overlayIcon} />}
-        {bars === 2 && <SignalCellularAlt2BarIcon className={classes.overlayIcon} />}
-        {bars === 1 && <SignalCellularAlt1BarIcon className={classes.overlayIcon} />}
-      </div>
+    <Tooltip title={title} componentsProps={{ tooltip: { sx: { background: '#000' } } }} placement="top">
+      <IconWrapper>
+        <BaseIcon />
+        <OverlayIcon>
+          {bars === 3 && <SignalCellularAltIcon />}
+          {bars === 2 && <SignalCellularAlt2BarIcon />}
+          {bars === 1 && <SignalCellularAlt1BarIcon />}
+        </OverlayIcon>
+      </IconWrapper>
     </Tooltip>
   )
 }
