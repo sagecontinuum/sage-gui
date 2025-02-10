1158571806ba123375a26e61df2b2d969d34d5cd
nconrad <1194246+nconrad@users.noreply.github.com>
2025-02-10 17:16:39 -0600

    add object download fiddle

---
+++ apps/sage/fiddle/test-download.tsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components'
+import { Card, CardViewStyle } from '/components/layout/Layout'
+
+
+export default function TestDownload() {
+
+  return (
+    <Root>
+      <CardViewStyle />
+
+      <Card >
+        <h2>Object Storage Example (W06C)</h2>
+        <img
+          src={
+            'https://storage.sagecontinuum.org/api/v1/data/imagesampler-top-2687/' +
+            'sage-imagesampler-top-0.3.7/000048b02d3ae335/1739206807613290074-sample.jpg'
+          } />
+      </Card>
+    </Root>
+  )
+}
+
+
+const Root = styled.div`
+  margin: 2rem;
+  max-width: 400px;
+
+  img {
+    max-width: 350px;
+  }
+`
