3a557ae13f9ceac95bfb6a694d8c44e314828300
nconrad <1194246+nconrad@users.noreply.github.com>
2025-02-14 16:39:30 -0600

    add bounding box option to image labeler

--- apps/admin/views/tests/BoundingBoxOverlay.tsx
+++ apps/admin/views/tests/BoundingBoxOverlay.tsx
@@ -6,48 +6,48 @@ const ImageContainer = styled.div`
   width: 350px;
 `
 
-const OverlayBox = styled.svg<{ top: number; left: number; width: number; height: number }>`
-  position: absolute;
-  top: ${props => props.top}px;
-  left: ${props => props.left}px;
-  width: ${props => props.width}px;
-  height: ${props => props.height}px;
-  border: 2px solid red;
-`
-
 const Image = styled.img`
   width: 100%;
 `
 
 interface BoundingBoxOverlayProps {
-  imageUrl: string;
-  box: {
-    top: number;
-    left: number;
-    width: number;
-    height: number;
-  };
+  src: string;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
 }
 
-const BoundingBoxOverlay: FC<BoundingBoxOverlayProps> = ({ imageUrl, box }) => {
+const BoundingBoxOverlay: FC<BoundingBoxOverlayProps> = ({ src, x, y, width, height }) => {
   return (
     <ImageContainer>
-      <Image src={imageUrl} alt="Overlay Image" />
-      <OverlayBox
-        top={box.top}
-        left={box.left}
-        width={box.width}
-        height={box.height}
-        viewBox={`0 0 ${box.width} ${box.height}`}
+      <Image src={src} />
+      <svg
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+        }}
         xmlns="http://www.w3.org/2000/svg"
       >
-        <rect width="100%" height="100%" fill="none" stroke="red" strokeWidth="2" />
-      </OverlayBox>
+        <rect
+          x={x}
+          y={y}
+          width={width}
+          height={height}
+          fill="none"
+          stroke="red"
+          strokeWidth="2"
+        />
+      </svg>
     </ImageContainer>
   )
 }
 
 export default BoundingBoxOverlay
+
 
 
 /*
--- apps/admin/views/tests/description-api.ts
+++ apps/admin/views/tests/description-api.ts
@@ -11,10 +11,16 @@ export type Description = {
     vsn: BK.VSN
     url: string
     file_size: number
+    image_height: number
+    image_width: number
+    bbox: BBox
   }[]
   text_was_extracted?: boolean
   unextracted_text?: string
 }
+
+export type BBox = [x1: number, y1: number, x2: number, y2: number]
+
 
 export function getDescriptions(file) : Promise<Description[]> {
   return fetch(`${url}/${file}`)
